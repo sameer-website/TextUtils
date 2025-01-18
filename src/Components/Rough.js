64666fdb939e3f4979e9f28f3927a8778402c7e2
sameer <iamsameer6805@gmail.com>
2025-01-18 14:52:06 +0530

    Add Initial Files

---
+++ src/Components/Rough.js
@@ -0,0 +1,25 @@
+const [btntext, setBtnText] = useState('Enable Dark Mode')
+
+const toggleStyle=()=>{
+  if(myStyle.color === 'black' && myStyle.backgroundColor === 'white' ){
+    setMyStyle({
+      color:'white',
+      backgroundColor:'black',
+      border:'1px solid white'
+    })
+    setBtnText('Enable Light Mode');
+  }
+  else{
+    setMyStyle({
+      color:'black',
+      backgroundColor:'white'
+    })
+    setBtnText('Enable Dark Mode');
+  }
+}
+
+const [myStyle, setMyStyle] = useState({
+    color:'black',
+    backgroundColor:'white'
+})
+
